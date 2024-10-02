0b0eac21f1c12d44715eb57a2f27f6618027f7ec
sebastian.siemianowski <rudph2@gmail.com>
2024-10-03 00:49:44 +0100

    This feature vastly improves the UX design and user experience including visual look of registrations, projects, and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
     else
       respond_to do |format|
         format.html { redirect_to @project, alert: 'Failed to add comment.' }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: 'comments/form', locals: { comment: @comment }) }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('comment_form', partial: 'comments/form', locals: { comment: @comment }) }
       end
     end
   end
